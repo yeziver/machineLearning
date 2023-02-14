d96ccfbb42a9a5568b86a7a4d03136b9e81127ff
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2023-02-14 14:52:47 -0500

    Update multiple_regression_predict.py

--- multiple_regression_predict.py
+++ multiple_regression_predict.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import pandas
 
-df = pandas.read("data.csv")
+df = pandas.read_csv("data.csv")
 
 X = df[['Weight', 'Volume']]
 Y = df['CO2']
@@ -11,4 +11,6 @@ Y = df['CO2']
 regr = linear_model.LinearRegression()
 regr.fit(X, Y)
 
-predict = regr.predict([[2300, 1088]])
+predict = regr.predict([[2300, 1300]])
+
+print(predict)
