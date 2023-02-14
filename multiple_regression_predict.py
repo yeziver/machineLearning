cf59dec181c86f87ed70a06bd9579f98ab0891e7
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2023-02-14 14:46:51 -0500

    Create multiple_regression_predict.py

---
+++ multiple_regression_predict.py
@@ -0,0 +1,14 @@
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+import pandas
+
+df = pandas.read("data.csv")
+
+X = df[['Weight', 'Volume']]
+Y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+predict = regr.predict([[2300, 1088]])
